47a1f6eb39681be3c02af2a505f440c584f2a78a
Galactuuus <galactuuus@gmail.com>
2021-09-23 19:34:10 +0200

    Basic Structure

---
+++ src/Containers/Home/Home.js
@@ -0,0 +1,15 @@
+import './Home.css';
+import Footer from '../../Components/Footer/Footer';
+import Navbar from '../../Components/NavBar/Navbar';
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <div>HOLA</div>
+            <Footer />
+        </>
+    );
+}
+
+export default Home
