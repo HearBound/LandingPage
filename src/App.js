4c925fb5aea69af4d6f17fd191acdde470234168
Davidmartcab <davidmartcab@gmail.com>
2021-10-23 13:03:15 +0200

    Add TryFree Responsive

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Containers/Home/Home.js';
 import About from './Containers/About/About';
 import Contact from './Containers/Contact/Contact';
+import TryFree from './Containers/TryFree/TryFree';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' component={Home} exact/>
+        <TryFree/>
+        {/* <Route path='/' component={Home} exact/>
         <Route path='/about' component={About} exact/>
-        <Route path='/contact' component={Contact} exact/>
+        <Route path='/contact' component={Contact} exact/> */}
       </Switch>
     </BrowserRouter>
   );
